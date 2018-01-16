78d35a88b936ae40313fda4c607e676d9b4017f0
Yubo Zhi <normanzyb@gmail.com>
2018-01-16 23:59:10 +0000

    Match meta tags

--- lyricshighlighter.cpp
+++ lyricshighlighter.cpp
@@ -3,35 +3,75 @@
 LyricsHighlighter::LyricsHighlighter(QTextDocument *parent) : QSyntaxHighlighter(parent) {}
 
 void LyricsHighlighter::highlightBlock(const QString &text) {
-    QTextCharFormat format;
     QRegularExpression expression;
     QRegularExpressionMatchIterator i;
 
     expression.setPatternOptions(QRegularExpression::MultilineOption);
 
-    // Time tag
-    format.setFontFamily("Courier");
-    format.setFontItalic(false);
-    format.setFontFixedPitch(true);
-    format.setForeground(Qt::blue);
-    format.setFontWeight(QFont::Light);
-    expression.setPattern("\\[[0-9:.]+\\]");
+    // Time tags
+    QTextCharFormat time_format;
+    time_format.setFontFamily("Courier");
+    time_format.setFontItalic(false);
+    time_format.setFontFixedPitch(true);
+    time_format.setForeground(Qt::blue);
+    time_format.setFontWeight(QFont::Light);
+    expression.setPattern("^(?:\\[[0-9:.]+\\])+");
+
     i = expression.globalMatch(text);
     while (i.hasNext()) {
       QRegularExpressionMatch match = i.next();
-      setFormat(match.capturedStart(), match.capturedLength(), format);
+      setFormat(match.capturedStart(), match.capturedLength(), time_format);
+    }
+
+    // Meta tags
+    QTextCharFormat meta_tag_format;
+    meta_tag_format.setFontFamily("Courier");
+    meta_tag_format.setFontItalic(false);
+    meta_tag_format.setFontFixedPitch(true);
+    meta_tag_format.setForeground(Qt::red);
+    meta_tag_format.setFontWeight(QFont::Light);
+    QTextCharFormat meta_info_format;
+    meta_info_format.setFontFamily(document()->defaultFont().family());
+    meta_info_format.setFontItalic(false);
+    meta_info_format.setFontFixedPitch(false);
+    meta_info_format.setForeground(Qt::darkRed);
+    meta_info_format.setFontWeight(QFont::Normal);
+
+    expression.setPattern("^\\[[^\\]:]*[^0-9\\]:]+[^\\]:]*:([^\\]]*)\\]");
+    i = expression.globalMatch(text);
+    while (i.hasNext()) {
+      QRegularExpressionMatch match = i.next();
+      setFormat(match.capturedStart(0), match.capturedLength(0), meta_tag_format);
+      setFormat(match.capturedStart(1), match.capturedLength(1), meta_info_format);
+    }
+
+    // Unknown tags
+    QTextCharFormat unknown_format;
+    unknown_format.setFontFamily(document()->defaultFont().family());
+    unknown_format.setFontItalic(false);
+    unknown_format.setFontFixedPitch(false);
+    unknown_format.setForeground(Qt::gray);
+    unknown_format.setFontWeight(QFont::Normal);
+
+    expression.setPattern("\\[[^:]*\\]");
+    i = expression.globalMatch(text);
+    while (i.hasNext()) {
+      QRegularExpressionMatch match = i.next();
+      setFormat(match.capturedStart(0), match.capturedLength(0), unknown_format);
     }
 
     // Lyric text
-    format.setFontFamily(document()->defaultFont().family());
-    format.setFontItalic(false);
-    format.setFontFixedPitch(false);
-    format.setForeground(Qt::black);
-    format.setFontWeight(QFont::Bold);
+    QTextCharFormat text_format;
+    text_format.setFontFamily(document()->defaultFont().family());
+    text_format.setFontItalic(false);
+    text_format.setFontFixedPitch(false);
+    text_format.setForeground(Qt::black);
+    text_format.setFontWeight(QFont::Bold);
+
     expression.setPattern("(?:^|\\])([^[\\]]+)(?:$|\\[)");
     i = expression.globalMatch(text);
     while (i.hasNext()) {
       QRegularExpressionMatch match = i.next();
-      setFormat(match.capturedStart(1), match.capturedLength(1), format);
+      setFormat(match.capturedStart(1), match.capturedLength(1), text_format);
     }
 }
